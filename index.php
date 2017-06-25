0f447d4f5f6e4080d52617d8e7598d62ba6f9dc1
jacob ruleaux <jakeruleaux@gmail.com>
2017-06-25 16:34:39 -0700

    build php bioler plate

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>friend list</title>
+</head>
+<body>
+    <h1>These is my peeps!</h1>
+    <ul>
+        <?php
+            foreach () {
+                echo "<li>""</li>";
+            }
+        ?>
+    </ul>
+</body>
+</html>
